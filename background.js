856c8d3080595a70fd0caa660668b4caf45965e0
riza <rizasabuncu@outlook.com>
2016-06-21 19:32:25 +0300

    Yeni animasyon ve restoran linki eklendi.

--- background.js
+++ background.js
@@ -1,10 +1,32 @@
 var sortColumn = $(".ys-reslist-sort");
-sortColumn.append('<br><br><button class="random-button random-button-xs" >Random</button><br><br><div class="alert-random alert-success-random randresult"><b> -> </b> <span></span></div>');
-
-$(".random-button").on('click',function() {
+sortColumn.append('<br><br><button class="random-button random-button-xs" >Random</button><br><br><div class="alert-random alert-success-random randresult"><b> -> </b> <a></a></div>');
 var items = [];
-	$('.ys-item').find('.head').find('.restaurantName').each(function(i,v) { items.push($(this).html()); });
+var speed = 100;
+var counter = 10;
+var action = true;
+$(".random-button").on('click',function() {
+	var action = true;
+	var counter = 10;
+	var interval = setInterval(randomAnimate, counter);
+	$('.ys-item').find('.head').find('.restaurantName').each(function(i,v) { items.push($(this).html() + "|" + "http://yemeksepeti.com" + $(this).attr("href")); });
 	var selectedItem = items[Math.floor(Math.random()*items.length)];
 	$(".randresult").fadeIn();
-	$(".randresult span").html(selectedItem);
+
+	var randomAnimate = function(){
+		if (action) {
+			if (counter == 150) {
+				action = false;
+			}
+			clearInterval(interval);
+			counter = counter + 10;
+			var selectedItem = items[Math.floor(Math.random()*items.length)];
+			$(".randresult a").html(selectedItem.split("|")[0]);
+			$(".randresult a").attr("href",selectedItem.split("|")[1]);
+			interval = setInterval(randomAnimate, counter);
+			console.log("A");
+		}
+	}
+	var interval = setInterval(randomAnimate, counter);
 });
+
+
